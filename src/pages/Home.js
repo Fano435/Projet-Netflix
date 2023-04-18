5bc3f084577d70605b111b22cf767f1aaa3cece8
Fano435 <108521943+Fano435@users.noreply.github.com>
2023-04-18 18:45:34 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,30 @@ import Infos from "../composant/infos-film";
 import { Link } from "react-router-dom";
 
 function Home() {
+  const [userData, setUserdata] = useState("");
+
+  const url =
+    "https://api.themoviedb.org/3/movie/550?api_key=d39ae299256eab37e526904cb2b272b3";
+  const getData = async (url) => {
+    const rawData = await fetch(url);
+    const jsonData = await rawData.json();
+    setUserdata(jsonData);
+  };
+
+  useEffect(() => {
+    getData(url);
+  }, []);
+  console.log(userData);
+
+  const dataKeys = Object.keys(userData);
   return (
     <div>
+      {userData.budget}
+      {dataKeys.map((key) => (
+        <h3>
+          {key} : {userData.key}
+        </h3>
+      ))}
       Je suis la page d'acceuil
       <Infos />
       <Link to={"/page"}>Lien vers la page détail</Link>
